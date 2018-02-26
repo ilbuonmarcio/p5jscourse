e2fd1bbe4d5ee7b57a90896d60a9c6d35f266637
marcioz98 <marciozgaming@gmail.com>
2018-02-26 21:27:46 +0100

    Changed exercise3 to rain

--- ex3/sketch.js
+++ ex3/sketch.js
@@ -1,4 +1,4 @@
-class Circle {
+class Drop {
   constructor(radius, x, y, xspeed, yspeed, color){
     this.radius = radius;
     this.x = x;
@@ -8,41 +8,49 @@ class Circle {
     this.color = color;
   }
 
-  hits(circle){
-    return sqrt(pow(this.x - circle.x, 2) + pow(this.y - circle.y, 2)) < this.radius + circle.radius;
-  }
-
   move(){
     this.x += this.xspeed;
     this.y += this.yspeed;
     if (this.x > width) this.x = 0;
+    if (this.x < 0) this.x = width;
     if (this.y > height) this.y = 0;
   }
 }
 
-var circles;
-var num_of_circles;
+var drops;
+var num_of_drops;
+var color;
 
 function setup() {
-  createCanvas(300, 300);
-  circles = new Array();
-  num_of_circles = 125;
-  for (var i = 0; i < num_of_circles; i++){
-    var color = [random()*255, random()*255, random()*255];
-    circles.push(new Circle(random()*25, random()*width, random()*height, random()*4, random()*3, color));
+  createCanvas(600, 600);
+  drops = new Array();
+  num_of_drops = 35;
+
+  color = [
+    120, 120, 255
+  ];
+
+  for (var i = 0; i < num_of_drops; i++){
+    drops.push(
+      new Drop(
+        random()*3,
+        random()*width,
+        random()*height,
+        random()*0.05,
+        random(0.3, 0.5),
+        color
+      )
+    );
   }
 }
 
 function draw() {
-  // background(175);
-  stroke(100);
-  for(var i = 0; i < num_of_circles; i++) {
-    for(var j = 0; j < num_of_circles; j++) {
-      if(circles[i].hits(circles[j]) && circles[i] !== circles[j]){
-        circles[i].move();
-      }
+  background(20, 20, 100);
+  for(var i = 0; i < num_of_drops; i++) {
+    for(var j = 0; j < num_of_drops; j++) {
+      drops[i].move();
+      stroke(drops[i].color)
+      ellipse(drops[i].x, drops[i].y, drops[i].radius);
     }
-    fill(circles[i].color);
-    ellipse(circles[i].x, circles[i].y, circles[i].radius);
   }
 }
